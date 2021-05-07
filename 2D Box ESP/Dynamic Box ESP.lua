1509ecc9b4bf413213fdbcd120726fa9f5790ae7
Cornyllius <53982126+Cornyllius@users.noreply.github.com>
2021-05-07 17:16:15 +0200

    Update Dynamic Box ESP.lua

--- 2D Box ESP/Dynamic Box ESP.lua
+++ 2D Box ESP/Dynamic Box ESP.lua
@@ -1,4 +1,5 @@
 -- Made by Blissful#4992
+-- Made by Blissful#4992
 local Player = game:GetService("Players").LocalPlayer
 local Camera = game:GetService("Workspace").CurrentCamera
 local Mouse = Player:GetMouse()
@@ -34,8 +35,8 @@ local function DrawESP(plr)
     local function Update()
         local c
         c = game:GetService("RunService").RenderStepped:Connect(function()
-            if plr.Character ~= nil and plr.Character:FindFirstChildOfClass("Humanoid") ~= nil and plr.Character:FindFirstChild("HumanoidRootPart") ~= nil and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 and plr.Character:FindFirstChild("Head") ~= nil then
-                local pos, vis = Camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
+            if plr.Character ~= nil and plr.Character:FindFirstChildOfClass("Humanoid") ~= nil and plr.Character.PrimaryPart ~= nil and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
+                local pos, vis = Camera:WorldToViewportPoint(plr.Character.PrimaryPart.Position)
                 if vis then 
                     local points = {}
                     local c = 0
@@ -43,22 +44,51 @@ local function DrawESP(plr)
                         if v:IsA("BasePart") then
                             c = c + 1
                             local p = Camera:WorldToViewportPoint(v.Position)
-                            if v.Name == "HumanoidRootPart" then
-                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, 0, -v.Size.Z)).p)
+                            if v == plr.Character.PrimaryPart then
+                                p, vis = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, 0, -v.Size.Z)).p)
                             elseif v.Name == "Head" then
-                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, v.Size.Y/2, v.Size.Z/1.25)).p)
+                                p, vis = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, v.Size.Y/2, v.Size.Z/1.25)).p)
                             elseif string.match(v.Name, "Left") then
-                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(-v.Size.X/2, 0, 0)).p)
+                                p, vis = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(-v.Size.X/2, 0, 0)).p)
                             elseif string.match(v.Name, "Right") then
-                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(v.Size.X/2, 0, 0)).p)
+                                p, vis = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(v.Size.X/2, 0, 0)).p)
                             end
-                            points[c] = p
+                            points[c] = {p, vis}
                         end
                     end
-                    local Left = GetClosest(points, Vector2.new(0, pos.Y))
-                    local Right = GetClosest(points, Vector2.new(Camera.ViewportSize.X, pos.Y))
-                    local Top = GetClosest(points, Vector2.new(pos.X, 0))
-                    local Bottom = GetClosest(points, Vector2.new(pos.X, Camera.ViewportSize.Y))
+
+                    local TopY = math.huge
+                    local DownY = -math.huge
+                    local LeftX = math.huge
+                    local RightX = -math.huge
+
+                    local Left
+                    local Right
+                    local Top
+                    local Bottom
+
+                    local closest = nil
+                    for _,v in pairs(points) do
+                        if v[2] == true then
+                            local p = v[1]
+                            if p.Y < TopY then
+                                Top = p
+                                TopY = p.Y
+                            end
+                            if p.Y > DownY then
+                                Bottom = p
+                                DownY = p.Y
+                            end
+                            if p.X > RightX then
+                                Right = p
+                                RightX = p.X
+                            end
+                            if p.X < LeftX then
+                                Left = p
+                                LeftX = p.X
+                            end
+                        end
+                    end
 
                     if Left ~= nil and Right ~= nil and Top ~= nil and Bottom ~= nil then
                         Box.PointA = Vector2.new(Right.X, Top.Y)
